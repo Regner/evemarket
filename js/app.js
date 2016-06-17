7eb18de355efb0cb0fdd6e65bf91c81a97b6a005
Regner Blok-Andersen <shadowdf@gmail.com>
2016-06-17 15:30:03 +0000

    Fixed some things and broke some other things

--- js/app.js
+++ js/app.js
@@ -10,23 +10,22 @@ Vue.component('item', {
   props: {
     model: Object
   },
-  
+
   data: function () {
     return {
       open: false
     }
   },
-  
+
   computed: {
     isFolder: function () {
-      return this.model.children &&
-        this.model.children.length
+      return this.model.isGroup
     }
   },
-  
+
   methods: {
     toggle: function () {
-      if (this.isFolder) {
+      if (this.model.isGroup) {
         this.open = !this.open
       }
     }
@@ -36,7 +35,7 @@ Vue.component('item', {
 
 new Vue({
   el: "#market",
-  
+
   data: {
     regions: {},
     regionsSorted: [],
@@ -48,24 +47,24 @@ new Vue({
     loadingProgress: 0,
     finishedLoading: false
   },
-  
+
   watch: {
     totalResources: function(value, oldValue) {
     },
-    
+
     loadedResources: function(value, oldValue) {
       this.loadingProgress = 100 * this.loadedResources / this.totalResources;
     },
-    
+
     finishedLoading: function(value, oldValue) {
       // console.log(this.finishedLoading);
     },
-    
+
     loadingProgress: function(value, oldValue) {
       if (this.totalResources >= 5000) {
         if (this.loadedResources == this.totalResources) {
           this.sortData();
-          
+
           this.finishedLoading = true;
         };
       };
@@ -74,7 +73,7 @@ new Vue({
 
   created: function() {
     this.$http.get(BASE_CREST_URL).then(function(response) {
-      this.getTypes(response.data.marketTypes.href);
+      // this.getTypes(response.data.marketTypes.href);
       this.getMarketGroups(response.data.marketGroups.href);
       this.getRegions(response.data.regions.href);
     });
@@ -84,21 +83,21 @@ new Vue({
     "getTypes": function(itemTypesURL) {
       this.crawlCREST(itemTypesURL, this.marketTypes, true);
     },
-    
+
     "getRegions": function(regionsURL) {
       this.crawlCREST(regionsURL, this.regions, true);
     },
-    
+
     "getMarketGroups": function(marketGroupsURL) {
       this.crawlCREST(marketGroupsURL, this.marketGroups, true);
     },
-    
+
     "crawlCREST": function(url, targetObj, firstPage = false) {
       this.$http.get(url).then(function(response) {
         if (firstPage) {
           this.totalResources += response.data.totalCount;
         }
-        
+
         for (var i = 0; i < response.data.items.length; i++) {
           targetObj[response.data.items[i].id] = response.data.items[i];
           this.loadedResources++
@@ -109,7 +108,7 @@ new Vue({
         }
       });
     },
-    
+
     "isFinishedLoading": function() {
       if (this.finishedLoading == true) {
         return true
@@ -117,32 +116,51 @@ new Vue({
         return false
       }
     },
-    
+
     "sortData": function() {
       this.sortMarketGroups();
       this.sortRegions();
-    },
-    
-    "sortMarketGroups": function () {
-      var test = [];
+      // this.sortMarketTypes();
+      // console.log(this.marketGroups);
 
+      // for (var groupID in this.marketGroups) {
+      //   this.marketGroups[parentGroupID].children.sort(function(a, b) {
+      //     sortCrestThings(a, b);
+      //   });
+      // };
+    },
+
+    "sortMarketGroups": function() {
       for (var groupID in this.marketGroups) {
         if (!this.marketGroups[groupID].hasOwnProperty("children")) {
           this.marketGroups[groupID].children = []
         };
+
+        this.marketGroups[groupID].isGroup = true;
       };
 
       for (var groupID in this.marketGroups) {
-
         if (this.marketGroups[groupID].hasOwnProperty("parentGroup")) {
           parentGroupID = this.marketGroups[groupID].parentGroup.id;
-          this.marketGroups[parentGroupID].children.push(this.marketGroups[groupID])
+          this.marketGroups[parentGroupID].children.push(this.marketGroups[groupID]);
         } else {
           this.marketGroupsSorted.push(this.marketGroups[groupID]);
         };
       };
+
+      this.marketGroupsSorted.sort(function(a, b) {
+        sortCrestThings(a, b);
+      });
     },
-    
+
+    "sortMarketTypes": function() {
+      for (var typeID in this.marketTypes) {
+        this.marketTypes[typeID].isGroup = false;
+        marketGroupID = this.marketTypes[typeID].marketGroup.id;
+        this.marketGroups[marketGroupID].children.push(this.marketTypes[typeID]);
+      };
+    },
+
     "sortRegions": function() {
       for (var regionID in this.regions) {
         // WH regions start at 11000000 and we want to skip them
@@ -150,35 +168,22 @@ new Vue({
           this.regionsSorted.push(this.regions[regionID])
         }
       }
-      
-      this.regionsSorted.sort(function(a, b){
-        var nameA=a.name.toLowerCase();
-        var nameB=b.name.toLowerCase();
-        
-        if (nameA < nameB)
-          return -1 
-        if (nameA > nameB)
-          return 1
-        return 0
+
+      this.regionsSorted.sort(function(a, b) {
+        sortCrestThings(a, b);
       })
     },
   },
 });
 
-function treeify(list) {
-    var treeList = [];
-    var lookup = {};
 
-    list.forEach(function(obj) {
-        lookup[obj["id"]] = obj;
-        obj["children"] = [];
-    });
-    list.forEach(function(obj) {
-        if (obj["parent"] != null) {
-            lookup[obj[parentAttr]][childrenAttr].push(obj);
-        } else {
-            treeList.push(obj);
-        }
-    });
-    return treeList;
+function sortCrestThings(a, b) {
+  var nameA=a.name.toLowerCase();
+  var nameB=b.name.toLowerCase();
+
+  if (nameA < nameB)
+    return -1
+  if (nameA > nameB)
+    return 1
+  return 0
 };
