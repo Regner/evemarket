983549033e605ade13b68186baa22ef7275f87c6
Regner Blok-Andersen <shadowdf@gmail.com>
2016-06-20 19:30:02 +0000

    Initial commit of the new world order

---
+++ src/components/Navigation.vue
@@ -0,0 +1,22 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+
+  props: {
+  },
+
+  data () {
+    return {
+    }
+  },
+
+  created () {
+  }
+}
+</script>
+
+<style>
+</style>
