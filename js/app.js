0a299877ecc2eb85696287d806463552079f2c1c
Regner Blok-Andersen <shadowdf@gmail.com>
2016-06-20 00:26:23 +0000

    Buy orders are now actually buy orders and sorted most expensive to least

--- js/app.js
+++ js/app.js
@@ -107,8 +107,8 @@ Vue.component('orders', {
 
 
         tmpBuyOrders.sort(function(a, b) {
-          if (a.price < b.price) return -1
-          if (a.price > b.price) return 1
+          if (a.price > b.price) return -1
+          if (a.price < b.price) return 1
           return 0
         })
 
